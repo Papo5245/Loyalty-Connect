7593f0299a87d05365f1644e79fab1d9a091126c
Papo5245 <juanpablovalles3@gmail.com>
2026-01-20 03:30:41 +0000

    Final Docker

--- server/index.ts
+++ server/index.ts
@@ -84,9 +84,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+  const port = Number(process.env.PORT) || 8080;
 
   httpServer.listen(port, "0.0.0.0", () => {
-    log(`serving on port ${port}`);
+    log(`Servidor iniciado en puerto ${port}`);
   });
 })();
